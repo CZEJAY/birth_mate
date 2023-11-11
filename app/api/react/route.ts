88c814cb0da9a0afde5f5e2aeb6cfbca5ffcb973
CZEJAY <calebjimmy67@gmail.com>
2023-11-11 17:47:03 +0100

    test 2

--- app/api/react/route.ts
+++ app/api/react/route.ts
@@ -1,16 +1,18 @@
 import { NextResponse, NextRequest } from "next/server"
 import User from "@/lib/models/user.model"
 import Thread from "@/lib/models/thread.model"
+import { connectToDB } from "@/lib/mongoose"
 
 export async function POST(req: NextRequest){
     try {
-        // const body = await req.json()
-        // const { postId, likesArray } = body
-        //  await Thread.findByIdAndUpdate(
-        //     postId,
-        //     { $set: { likes: likesArray } },
-        //     { new: true }
-        // );
+        await connectToDB()
+        const body = await req.json()
+        const { postId, likesArray } = body
+         await Thread.findByIdAndUpdate(
+            postId,
+            { $set: { likes: likesArray } },
+            { new: true }
+        );
 
         return NextResponse.json({message: "Updated"}, {status: 200})
     } catch (error) {
